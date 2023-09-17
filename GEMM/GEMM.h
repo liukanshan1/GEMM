5269bfeefff5f50d7010430192108ffc99d84799
FutabaRio965 <85719360+liukanshan1@users.noreply.github.com>
2023-09-17 12:50:16 +0800

    header and test

--- GEMM/GEMM.h
+++ GEMM/GEMM.h
@@ -1,3 +1,104 @@
 #pragma once
+#include <random>
+#include <iostream>
 int M, N, K;
-int m, n, k;
+
+// 初始化参数
+int m = 30;
+int n = 30;
+int k = 30;
+
+enum class MatrixType
+{
+	RowMajor,
+	ColMajor
+};
+
+//随机生成M，N，K
+void random_M_N_K()
+{
+	std::default_random_engine e;
+	std::uniform_int_distribution<unsigned> u1(1, 1000);
+	std::uniform_int_distribution<unsigned> u2(5000, 10000);
+	M = u1(e);
+	N = u1(e);
+	K = u2(e);
+}
+
+//随机生成矩阵
+void random_matrix(double* matrix, int row, int col)
+{
+	std::default_random_engine e;
+	std::uniform_real_distribution<double> u(-100, 100);
+	for (int i = 0; i < row * col; i++)
+	{
+		matrix[i] = u(e);
+	}
+}
+
+//Row major 转 col major
+void row_to_col(double* matrix, int row, int col)
+{
+	double* temp = new double[row * col];
+	memccpy(temp, matrix, row * col, sizeof(double));
+	for (int i = 0; i < row; i++)
+	{
+		for (int j = 0; j < col; ++j)
+		{
+			matrix[j * row + i] = temp[i * col + j];
+		}
+	}
+	delete[] temp;
+}
+
+// Col major 转 row major
+void col_to_row(double* matrix, int row, int col)
+{
+	double* temp = new double[row * col];
+	memccpy(temp, matrix, row * col, sizeof(double));
+	for (int i = 0; i < row; i++)
+	{
+		for (int j = 0; j < col; ++j)
+		{
+			matrix[i * col + j] = temp[j * row + i];
+		}
+	}
+	delete[] temp;
+}
+
+void print_matrix(double* matrix, int row, int col, MatrixType type) {
+if (type == MatrixType::RowMajor)
+	{
+		for (int i = 0; i < row; ++i)
+		{
+			std::cout << "[";
+			for (int j = 0; j < col; ++j)
+			{
+				std::cout << matrix[i * col + j] << " ";
+			}
+			std::cout << "]" << std::endl;
+		}
+	}
+	else
+	{
+		for (int i = 0; i < row; ++i)
+		{
+			std::cout << "[";
+			for (int j = 0; j < col; ++j)
+			{
+				std::cout << matrix[j * row + i] << " ";
+			}
+			std::cout << "]" << std::endl;
+		}
+	}
+}
+
+void print_vector(double* vector, int row)
+{
+	for (int i = 0; i < row; ++i)
+	{
+		std::cout << vector[i] << " ";
+	}
+	std::cout << std::endl;
+}
+		
--- Tests/test.cpp
+++ Tests/test.cpp
@@ -1,6 +1,22 @@
 #include "pch.h"
+#include "../GEMM/GEMM.h"
 
-TEST(TestCaseName, TestName) {
-  EXPECT_EQ(1, 1);
-  EXPECT_TRUE(true);
+TEST(Header, GenMNK) {
+	random_M_N_K();
+	EXPECT_LE(M, 1000);
+	EXPECT_LE(N, 1000);
+	EXPECT_LE(K, 10000);
+	EXPECT_GT(M, 0);
+	EXPECT_GT(N, 0);
+	EXPECT_GT(K, 0);
+	EXPECT_TRUE(true)<< M<<" " << N <<" " << K << " ";
+}
+
+TEST(Header, MatrixGen) {
+	double* matrix = new double[100];
+	random_matrix(matrix, 10, 10);
+	for (int i = 0; i < 100; i++) {
+		EXPECT_LE(matrix[i], 100);
+		EXPECT_GE(matrix[i], -100);
+	}
 }
